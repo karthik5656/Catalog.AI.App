0258ca1cfc376ef07a86292ab91d3bd0141d62df
karthik5656 <61219228+karthik5656@users.noreply.github.com>
2026-08-30 01:55:10 +0530

    Added Templates for Catalog API and Gateway

---
+++ src/backend/Catalog.API/Catalog.API/Startup.cs
@@ -0,0 +1,47 @@
+﻿namespace Catalog.API;
+
+public class Startup
+{
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers();
+
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+    }
+
+    public void Configure(
+        IApplicationBuilder app,
+        IWebHostEnvironment environment)
+    {
+        if (environment.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "Catalog API v1");
+                options.RoutePrefix = "swagger";
+            });
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+    }
+}
